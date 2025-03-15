4c33e31be28927751fb1cabf128abf665a0235d5
Surya Chandra <suryavijetha123@gmail.com>
2025-03-15 18:07:37 +0530

    feat:Connections Page

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import feedReducer from "./feedSlice.js";
+import connectionsReducer from "./connectionsSlice.js"
 const appStore=configureStore({
     reducer:{
         // add your reducers here
         user:userReducer,
         feed:feedReducer,
+        connections:connectionsReducer,
     }
 });
 export default appStore;
